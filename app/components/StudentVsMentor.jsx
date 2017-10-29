31ef398164641f90cdf39d409eee0c0771ac1c8a
tfaieta <tfaieta@gmail.com>
2017-10-29 09:40:36 -0400

    f
    f

--- app/components/StudentVsMentor.jsx
+++ app/components/StudentVsMentor.jsx
@@ -25,12 +25,12 @@ export default class StudentVsMentor extends Component {
 
         return (
             <div>
-                <button onClick = { this.StudentClick }>Student</button>
-
-
-                <button onClick = { this.MentorClick }>Mentor</button>
+                <h1 className="todoapp">Which One Are You?</h1>
+                <button className="button" onClick = { this.StudentClick }>Student</button>
+                <button className="button" onClick = { this.MentorClick }>Mentor</button>
             </div>
-
         );
     }
 }
+
+

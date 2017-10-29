1307d75e613cd4bd3eb795da429284b1ac43cdc0
Daniel Lobaton <dlobaton@ufl.edu>
2017-10-29 02:23:04 -0400

    Data is now being saved

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import StudentForm from './Components/StudentForm/StudentForm.js';
 import './App.css';
 
 class App extends Component {
@@ -8,11 +9,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Hi</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <StudentForm/>
       </div>
     );
   }
--- src/Components/StudentForm/StudentForm.js
+++ src/Components/StudentForm/StudentForm.js
@@ -1,19 +1,48 @@
-/*import React, { Component } from 'react';
+import React, { Component } from 'react';
 
-class StudentForm extends Component
+export default class StudentForm extends Component
 {
-    render()
-    {
-        <div id ='Layout'>
-            <div className='pure-g'>
-                <form className='pure-form'>
-                    <fieldset>
-                        <legend>Student Information</legend>
-                        <input type='email' placeholder='morty@pickles.com'/>
-                        <input type='text' placeholder='Phone Number'/>
-                        <input type='text' placeholder='First Name'/>
-                        <input type='text' placeholder='Last Name'/>
-                    </fieldset>
+    constructor(props) {
+        super(props);
+        this.state =
+            {
+                studentEmail: '',
+                studentPhone: '',
+                studentFirst: '',
+                studentLast: ''
+            }
     }
 
-} */
+    handleEmailChange = event => {
+        this.setState({ studentEmail: event.target.value });
+    }
+    handlePhoneChange = event => {
+        this.setState({ studentPhone: event.target.value });
+    }
+    handleFirstChange = event => {
+        this.setState({ studentFirst: event.target.value });
+    }
+    handleLastChange = event => {
+        this.setState({ studentLast: event.target.value });
+    }
+
+    render() {
+
+        return (
+            <div id ='Layout'>
+                <div className='pure-g'>
+                    <form className='pure-form'>
+                        <fieldset>
+                            <legend>Student Information</legend>
+                            <input type='email' placeholder='morty@pickles.com' value={this.state.studentEmail} onChange={this.handleEmailChange}/>
+                            <input type='text' placeholder='Phone Number'value={this.state.studentPhone} onChange={this.handlePhoneChange}/>
+                            <input type='text' placeholder='First Name' value={this.state.studentFirst} onChange={this.handleFirstChange}/>
+                            <input type='text' placeholder='Last Name'value={this.state.studentLast} onChange={this.handleLastChange}/>
+                            <button type='submit' className = 'pure-button pure-button-primary'>Submit</button>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}

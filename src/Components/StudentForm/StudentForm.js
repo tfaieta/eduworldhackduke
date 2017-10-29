3a40e49fa3e30901c426cc3a4d3120fd75fd558d
Daniel Lobaton <dlobaton@ufl.edu>
2017-10-28 23:45:40 -0400

    Basic form set

---
+++ src/Components/StudentForm/StudentForm.js
@@ -0,0 +1,19 @@
+/*import React, { Component } from 'react';
+
+class StudentForm extends Component
+{
+    render()
+    {
+        <div id ='Layout'>
+            <div className='pure-g'>
+                <form className='pure-form'>
+                    <fieldset>
+                        <legend>Student Information</legend>
+                        <input type='email' placeholder='morty@pickles.com'/>
+                        <input type='text' placeholder='Phone Number'/>
+                        <input type='text' placeholder='First Name'/>
+                        <input type='text' placeholder='Last Name'/>
+                    </fieldset>
+    }
+
+} */
